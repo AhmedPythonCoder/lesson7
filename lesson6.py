bd25e0870b6aca59f131a411d6a3e2948f963b0c
AhmedPythonCoder <122676257+AhmedPythonCoder@users.noreply.github.com>
2024-12-26 19:07:18 +0100

    Add files via upload

---
+++ lesson6.py
@@ -0,0 +1,19 @@
+#IMPORT LIBRARIES
+import pgzrun
+
+#set the dimentions of the screen
+WIDTH = 500
+HEIGHT = 700
+#set the title of the screen
+TITLE = 'rectangle'
+
+
+
+
+def draw():
+    #Rect(x,y,width,height)
+    #screen.draw.circle(pos,radius,(r,g,b))
+    rect()
+
+#draw()
+pgzrun.go()
